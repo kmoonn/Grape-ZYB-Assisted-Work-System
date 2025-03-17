fe2c0d728d91e684eabd13792d105126f674145b
Hu shan <103509070+kmoonn@users.noreply.github.com>
2025-03-17 15:29:01 +0800

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -5,8 +5,6 @@ from contextlib import contextmanager
 
 DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/grape"
 
-# DATABASE_URL = "mysql+pymysql://grape:tYmEEcdjKY2m6mM3@47.94.107.234:3306/grape"
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
