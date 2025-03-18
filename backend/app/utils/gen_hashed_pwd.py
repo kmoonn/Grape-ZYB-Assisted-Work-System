0282a3a9c3092f16cc00064614b66af81bae4d84
Hu shan <hushan@kmoon.fun>
2025-03-18 21:43:03 +0800

    Update gen_hashed_pwd.py

--- backend/app/utils/gen_hashed_pwd.py
+++ backend/app/utils/gen_hashed_pwd.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-hashed_password = pwd_context.hash("guoyujie1011")
+hashed_password = pwd_context.hash("123456")
 
 print(hashed_password)
